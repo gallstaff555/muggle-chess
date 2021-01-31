04d4eabc0a1027b9dbfd73b6a375681e5051a747
david <davidsolo555@hotmail.com>
2021-01-31 14:40:40 -0800

    production config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 const config = {
-    "backendURL": "http://localhost:4001",
-    //"backendURL": "https://mugglechess.azurewebsites.net",
+    //auth0 domain
     auth0domain: "dev-490elg7s.us.auth0.com",
-    auth0clientID: "9PRB8rdHxMCZRVoVKiTktM6GdLbqYorQ",
+
+    /* test */
+    //"backendURL": "http://localhost:4001",
+    //auth0clientID: "9PRB8rdHxMCZRVoVKiTktM6GdLbqYorQ",
+
+    /* live */
+    "backendURL": "https://mugglechess.azurewebsites.net",
+    auth0clientID: "EjCFbmU6oFyOPejgPat60ww9Ru6srhD1",
 };
 
 module.exports = config;
